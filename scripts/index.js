e72b6cd0ab309adea3759d0295017fcd1bbf0bc3
Alexander <alex@alexvoiceover.com>
2023-11-04 18:28:10 +0000

    created empty functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,12 @@
 // Definition of functions and JS related with DOM manipulation
-export function func1(){}
+export function addTask(group,task){
+
+}
+
+export function deleteTask(task){
+    
+}
+
+export function moveTask(origin, end, task){
+    
+}
--- scripts/index.test.js
+++ scripts/index.test.js
@@ -3,22 +3,22 @@ import { equal, notEqual, test } from './test-helpers.js'
 import { func1 } from './index.js'
 
 // Add tasks to a list so that I can keep track of them
-test("Submitting a new task adds it to the list", () => {
-    // test goes here
-  });
+test('Submitting a new task adds it to the list', () => {
+  // test goes here
+})
 
 //Check things off my list so that I can see what I’ve done
-test("Checking an entry marks it as complete", () => {
-// test goes here
-});
+test('Checking an entry marks it as complete', () => {
+  // test goes here
+})
 
 //Delete things from the list if I don’t need to do them anymore
-test("Deleting an entry removes it from the list", () => {
-// test goes here
-});
+test('Deleting an entry removes it from the list', () => {
+  // test goes here
+})
 
 //Strech
 //Filter out completed to-dos from my list so that I can focus on what’s left to do
-test("Toggling the filter hides completed tasks from the list", () => {
-    // test goes here
-});
+test('Toggling the filter hides completed tasks from the list', () => {
+  // test goes here
+})
