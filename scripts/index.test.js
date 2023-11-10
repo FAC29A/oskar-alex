9c1f80613ab1b2817693f9ea4a52941f4cd0a5e9
Alexander <alex@alexvoiceover.com>
2023-11-10 12:50:53 +0000

    Test for send task to Completed Fixes #12

--- scripts/index.test.js
+++ scripts/index.test.js
@@ -1,6 +1,6 @@
 //Collection of tests to be performed
 import { equal, notEqual, test } from './test-helpers.js';
-
+/*
 test('Submitting a new task adds it to the list', async () => {
   await new Promise((resolve) => {
     setTimeout(() => {
@@ -22,8 +22,8 @@ test('Submitting a new task adds it to the list', async () => {
     }, 100);
   });
 });
-
-
+*/
+/*
 test('Adding a task using Enter', async () => {
   await new Promise((resolve) => {
     setTimeout(() => {
@@ -43,36 +43,77 @@ test('Adding a task using Enter', async () => {
 
       equal(taskText.trim(), 'Task Create with Enter Test')
 
-      inputField.value = '';
+      inputField.value = ''
 
       resolve() // Resolve the promise after the test completes
-      console.groupEnd('Adding a task using Enter');
+      console.groupEnd('Adding a task using Enter')
     }, 100)
   })
 })
-
+*/
+/*
 test('Creating a new group adds it to the group list', async () => {
   await new Promise((resolve) => {
     setTimeout(() => {
       console.group('Creating a new group adds it to the group list')
 
       // Corrected the selector to use ID instead of class
-      const createGroupButton = document.querySelector('#createGroupButton');
-      const groupList = document.querySelector('.groupsContainer');
+      const createGroupButton = document.querySelector('#createGroupButton')
+      const groupList = document.querySelector('.groupsContainer')
       
-      createGroupButton.click();
-      const addedGroup = groupList.lastElementChild; // Get the last group element added
+      createGroupButton.click()
+      const addedGroup = groupList.lastElementChild // Get the last group element added
       // Set the group name
-      const groupTitleInput = addedGroup.querySelector('.groupTitle');
-      groupTitleInput.value = 'Test Group';
+      const groupTitleInput = addedGroup.querySelector('.groupTitle')
+      groupTitleInput.value = 'Test Group'
 
       //Check
-      const testGroup = groupList.lastElementChild;
-      const testgroupTitleInput = testGroup.querySelector('.groupTitle');
-      const groupName = testgroupTitleInput.value;
-      equal(groupName.trim(), 'Test Group');
-      resolve(); // Resolve the promise after the test completes
-      console.groupEnd('Creating a new group adds it to the group list');
+      const testGroup = groupList.lastElementChild
+      const testgroupTitleInput = testGroup.querySelector('.groupTitle')
+      const groupName = testgroupTitleInput.value
+      equal(groupName.trim(), 'Test Group')
+      resolve() // Resolve the promise after the test completes
+      console.groupEnd('Creating a new group adds it to the group list')
     }, 100);
   });
 });
+*/
+
+test('Sending a task to completed moves it to the completed section', async () => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      console.group('Sending a task to completed moves it to the completed section')
+
+      // Step 1: Create a new task
+      const inputField = document.querySelector('.addTaskField')
+      const addTaskButton = document.querySelector('.addTaskButton')
+      inputField.value = 'Task for Completion Test'
+      addTaskButton.click()
+
+      setTimeout(() => {
+        // Step 2: Find and click the 'send to complete' button for the new task
+        const taskList = document.querySelector('#listToDo')
+        const newTask = taskList.lastElementChild
+        const moveToCompletedButton = newTask.querySelector('.moveToCompleted')
+        moveToCompletedButton.click()
+
+        setTimeout(() => {
+          // Step 3: Check if the task is now in the completed section
+          const completedList = document.querySelector('#completedList')
+          const completedTask = completedList.lastElementChild
+
+          // Adjust this line based on the actual structure of the completed task
+          const completedTaskText = completedTask ? completedTask.textContent.trim() : null
+
+          equal(completedTaskText.trim(), 'Task for Completion Test')
+
+          inputField.value = '' // Clean up
+
+          resolve()
+
+          console.groupEnd('Sending a task to completed moves it to the completed section')
+        }, 100)
+      }, 100)
+    }, 100)
+  })
+})
