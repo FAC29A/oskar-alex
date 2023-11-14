38b2eed4ab2b1dd7bc5ee927eea5a2c75e1f9fd9
Alexander <alex@alexvoiceover.com>
2023-11-14 13:25:16 +0000

    Modif instructions

--- scripts/index.data.js
+++ scripts/index.data.js
@@ -3,7 +3,7 @@ export const deafultGroups = [
     backgroundColour: "#D0E6FF"},
     {groupName: "In Progress",
     backgroundColour: "#FFD580"},
-    {groupName: "Instructions - delete →",
+    {groupName: "Instructions",
     backgroundColour: "#E7FFD6",
     Tasks: ['← key moves task to next group', '→ key moves task to previous group', 'Delete key for deleting task', 'Enter to move to completed']},
 ]
